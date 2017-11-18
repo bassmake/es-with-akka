328c135da5a10c3eb4fe4faeef259115e5bda82d
Miroslav Matejovsky <miroslav.matejovsky@wirecard.com>
2017-11-18 22:31:10 +0100

    CustomerRepository

--- 03-akka-persistence-query/src/main/resources/db/migration/V2__Create_customers_table.sql
+++ 03-akka-persistence-query/src/main/resources/db/migration/V2__Create_customers_table.sql
@@ -2,14 +2,15 @@ DROP TABLE IF EXISTS PUBLIC."customers";
 
 CREATE TABLE IF NOT EXISTS PUBLIC."customer_accounts" (
   "username" VARCHAR(255) PRIMARY KEY,
-  "point_balance" INT NOT NULL
+  "point_balance" INT NOT NULL,
+  "created_at" TIMESTAMP NOT NULL,
+  "updated_at" TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS PUBLIC."vouchers" (
   "code" VARCHAR(255) NOT NULL PRIMARY KEY,
   "username" VARCHAR(255) NOT NULL,
-  "points" INT NOT NULL,
-  "value" DECIMAL(2) NOT NULL,
+  "value" DOUBLE NOT NULL,
   FOREIGN KEY ("username") REFERENCES PUBLIC."customer_accounts"("username")
 );
 
